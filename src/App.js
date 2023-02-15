44c9fac6a7b1ecf317d372920fdf111d95bc9656
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-15 22:14:57 +0600

    input

--- src/App.js
+++ src/App.js
@@ -6,19 +6,30 @@ function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <div>
-      <h1 className="text-blue-500">Register</h1>
+    <div className="p-10">
+      <h1 className="text-xl font-bold my-10">Register</h1>
       <div>
         <form>
-          <input 
-          type="text"
-           />
-          <input 
-          type="text"
-           />
-          <input 
-          type="text"
-           />
+          <input
+            className="my-10"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br />
+          <input
+            className="my-10"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <input
+            className="my-10"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </form>
       </div>
     </div>
