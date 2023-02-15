9a81efca515d9fba96706441db343d98da75e31e
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-15 22:10:09 +0600

    States

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div>
-     <h1 className='text-blue-500'>Text</h1>
+      <h1 className="text-blue-500">Register</h1>
+      <div>
+        <form></form>
+      </div>
     </div>
   );
 }
