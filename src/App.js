e1e79acced3b190c4692dc3c0efe46ccdf00daf2
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-17 22:28:41 +0600

    register

--- src/App.js
+++ src/App.js
@@ -1,54 +1,11 @@
-import { useState } from "react";
 import "./App.css";
+import Register from "./pages/Register";
 
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  async function registerUser(e) {
-    e.preventDefault();
-    const res = await fetch("http://localhost:5000/api/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name, email, password }),
-    });
-    const data = await res.json();
-    console.log(data);
-  }
   return (
-    <div className="p-10">
-      <h1 className="text-xl font-bold my-10">Register</h1>
+    <div>
       <div>
-        <form onSubmit={registerUser}>
-          <input
-            className="my-3 py-2 px-5 rounded-sm bg-gray-300"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <br />
-          <input
-            className="my-3 py-2 px-5 rounded-sm bg-gray-300"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <br />
-          <input
-            className="my-3 py-2 px-5 rounded-sm bg-gray-300"
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <br />
-          <input
-            type="submit"
-            value="Register"
-            className="bg-indigo-700 px-10 py-3 text-white font-bold rounded-sm"
-          />
-        </form>
+        <Register/>
       </div>
     </div>
   );
